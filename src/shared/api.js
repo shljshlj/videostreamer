c862a2ba95f1a8fa3226dda50a72606c038adf29
shljshlj <34565607+shljshlj@users.noreply.github.com>
2019-07-15 23:09:13 +0200

    api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { YT_API_KEY, BASE_API_ENDPOINT } from './constants';
 
-console.log(YT_API_KEY)
-
 export default axios.create({
     baseURL: BASE_API_ENDPOINT,
     params: {
